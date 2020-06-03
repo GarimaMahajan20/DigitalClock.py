035fd9fcc2a95271ba7a2ca0b30d8e1b45daa064
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-06-04 00:51:27 +0530

    Digital Clock

---
+++ clock.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+r= Tk()
+r.title('Clock')
+r.minsize(300,300)
+def time():
+    s =strftime('%d-%B-%Y , %H:%M:%S %p')
+    l.config(text= s)
+    l.after(1000,time)
+l =Label(r,font=('calibri',40 , 'bold'),background ='purple' , foreground ='white')
+l.pack(anchor ='center')
+time()
+def exit():
+    r.destroy()
+exit =Button(r,text='Exit',command = exit)
+exit.pack(side=BOTTOM , anchor='e')
+
+mainloop()
